9fd31fdf2adb0d47694cf9d7caba4e2ca2dd1f4e
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-06-11 00:39:41 +0200

    soong: Add TARGET_CAMERA_BOOTTIME_TIMESTAMP conditional
    Change-Id: Ic48acda208fc9990fc5c9159ad06518d48f21477
    Signed-off-by: HeroBuxx <me@buxxed.tk>

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -30,6 +30,7 @@ SOONG_CONFIG_NAMESPACES += halcyonGlobalVars
 SOONG_CONFIG_halcyonGlobalVars += \
     additional_gralloc_10_usage_bits \
     gralloc_handle_has_reserved_size \
+    needs_camera_boottime \
     target_init_vendor_lib \
     target_ld_shim_libs \
     target_surfaceflinger_udfps_lib \
@@ -56,6 +57,7 @@ endif
 # Soong bool variables
 SOONG_CONFIG_halcyonGlobalVars_gralloc_handle_has_reserved_size := $(TARGET_GRALLOC_HANDLE_HAS_RESERVED_SIZE)
 SOONG_CONFIG_halcyonGlobalVars_uses_egl_display_array := $(TARGET_USES_EGL_DISPLAY_ARRAY)
+SOONG_CONFIG_halcyonGlobalVars_needs_camera_boottime := $(TARGET_CAMERA_BOOTTIME_TIMESTAMP)
 SOONG_CONFIG_halcyonNvidiaVars_uses_nvidia_enhancements := $(NV_ANDROID_FRAMEWORK_ENHANCEMENTS)
 SOONG_CONFIG_halcyonQcomVars_supports_extended_compress_format := $(AUDIO_FEATURE_ENABLED_EXTENDED_COMPRESS_FORMAT)
 SOONG_CONFIG_halcyonQcomVars_uses_pre_uplink_features_netmgrd := $(TARGET_USES_PRE_UPLINK_FEATURES_NETMGRD)
