87d200a38989b7febb0686d7339016cacf24ae6c
jhenrique09 <jhsv09@gmail.com>
2023-07-11 20:27:13 -0300

    halcyon: vendor: Add TARGET_CAMERA_USES_NEWER_HIDL_OVERRIDE_FORMAT
    Change-Id: I0b5ded178ec35f87f768509e497ecce4926d4336
    Signed-off-by: HeroBuxx <me@herobuxx.me>

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -37,6 +37,7 @@ SOONG_CONFIG_halcyonGlobalVars += \
     bootloader_message_offset \
     gralloc_handle_has_reserved_size \
     needs_camera_boottime \
+    camera_uses_newer_hidl_override_format \
     target_init_vendor_lib \
     target_ld_shim_libs \
     target_surfaceflinger_udfps_lib \
@@ -66,6 +67,7 @@ endif
 SOONG_CONFIG_halcyonGlobalVars_camera_needs_client_info := $(TARGET_CAMERA_NEEDS_CLIENT_INFO)
 SOONG_CONFIG_halcyonGlobalVars_camera_needs_client_info_lib := $(TARGET_CAMERA_NEEDS_CLIENT_INFO_LIB)
 SOONG_CONFIG_halcyonGlobalVars_camera_needs_client_info_lib_oplus := $(TARGET_CAMERA_NEEDS_CLIENT_INFO_LIB_OPLUS)
+SOONG_CONFIG_halcyonGlobalVars_camera_uses_newer_hidl_override_format := $(TARGET_CAMERA_USES_NEWER_HIDL_OVERRIDE_FORMAT)
 SOONG_CONFIG_halcyonGlobalVars_gralloc_handle_has_reserved_size := $(TARGET_GRALLOC_HANDLE_HAS_RESERVED_SIZE)
 SOONG_CONFIG_halcyonGlobalVars_uses_egl_display_array := $(TARGET_USES_EGL_DISPLAY_ARRAY)
 SOONG_CONFIG_halcyonGlobalVars_needs_oplus_tag := $(TARGET_NEEDS_OPLUS_VENDOR_TAG)
