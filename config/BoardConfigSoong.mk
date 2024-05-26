b9c368c074bf06ad2d6741ee9b2a4935e087a8d4
Dhina17 <dhinalogu@gmail.com>
2024-05-26 15:27:50 +0530

    halcyon: soong: Add libcameraservice extension config
    Change-Id: Id20f782c1008fed1f15250129618326091f61bf8
    Signed-off-by: HeroBuxx <me@herobuxx.me>

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -55,6 +55,7 @@ SOONG_CONFIG_halcyonGlobalVars += \
     target_health_charging_control_supports_limit \
     target_health_charging_control_supports_toggle \
     target_init_vendor_lib \
+    target_libcameraservice_ext_lib \
     target_inputdispatcher_skip_event_key \
     target_power_libperfmgr_mode_extension_lib \
     target_surfaceflinger_udfps_lib \
@@ -95,6 +96,7 @@ SOONG_CONFIG_halcyonGlobalVars_uses_legacy_fd_fbdev := $(TARGET_USES_LEGACY_FD_F
 BOOTLOADER_MESSAGE_OFFSET ?= 0
 TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS ?= 0
 TARGET_CAMERA_OVERRIDE_FORMAT_FROM_RESERVED ?= false
+TARGET_CAMERA_SERVICE_EXT_LIB ?= libcameraservice_ext_lib
 TARGET_GRALLOC_HANDLE_HAS_CUSTOM_CONTENT_MD_RESERVED_SIZE ?= false
 TARGET_GRALLOC_HANDLE_HAS_RESERVED_SIZE ?= false
 TARGET_GRALLOC_HANDLE_HAS_UBWCP_FORMAT ?= false
@@ -122,6 +124,7 @@ SOONG_CONFIG_halcyonGlobalVars_target_camera_package_name := $(TARGET_CAMERA_PAC
 SOONG_CONFIG_halcyonGlobalVars_disable_bluetooth_le_read_buffer_size_v2 := $(TARGET_DISABLE_BLUETOOTH_LE_READ_BUFFER_SIZE_V2)
 SOONG_CONFIG_halcyonGlobalVars_disable_bluetooth_le_set_host_feature := $(TARGET_DISABLE_BLUETOOTH_LE_SET_HOST_FEATURE)
 SOONG_CONFIG_halcyonGlobalVars_target_init_vendor_lib := $(TARGET_INIT_VENDOR_LIB)
+SOONG_CONFIG_halcyonGlobalVars_target_libcameraservice_ext_lib := $(TARGET_CAMERA_SERVICE_EXT_LIB)
 SOONG_CONFIG_halcyonGlobalVars_target_inputdispatcher_skip_event_key := $(TARGET_INPUTDISPATCHER_SKIP_EVENT_KEY)
 SOONG_CONFIG_halcyonGlobalVars_target_power_libperfmgr_mode_extension_lib := $(TARGET_POWER_LIBPERFMGR_MODE_EXTENSION_LIB)
 SOONG_CONFIG_halcyonGlobalVars_target_surfaceflinger_udfps_lib := $(TARGET_SURFACEFLINGER_UDFPS_LIB)
