edecfdacbd4f4e56fd6bbc170f6e478f45b404a9
LuK1337 <priv.luk@gmail.com>
2024-11-04 20:24:31 +0100

    halcyon: config: Add text classifier files to allowed product artifacts
    Signed-off-by: HeroBuxx <me@herobuxx.me>

--- config/common.mk
+++ config/common.mk
@@ -220,6 +220,12 @@ PRODUCT_PACKAGES += \
     libtextclassifier_actions_suggestions_universal_model \
     libtextclassifier_lang_id_model
 
+PRODUCT_ARTIFACT_PATH_REQUIREMENT_ALLOWED_LIST += \
+    system/etc/textclassifier/actions_suggestions.universal.model \
+    system/etc/textclassifier/lang_id.model \
+    system/etc/textclassifier/textclassifier.en.model \
+    system/etc/textclassifier/textclassifier.universal.model
+
 # Enable support of one-handed mode
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.support_one_handed_mode=true
