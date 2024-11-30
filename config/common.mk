997abe3069cc68b9590f1a20bd096506bb86b311
Adithya R <gh0strider.2k18.reborn@gmail.com>
2024-11-30 16:25:16 +0800

    halcyon: Build LMOFreeform and Sidebar
    Signed-off-by: HeroBuxx <me@herobuxx.me>

--- config/common.mk
+++ config/common.mk
@@ -223,6 +223,11 @@ PRODUCT_SYSTEM_EXT_PROPERTIES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.biometrics.face.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.biometrics.face.xml
 
+# Freeform
+PRODUCT_PACKAGES += \
+    LMOFreeform \
+    LMOFreeformSidebar
+
 # Messaging
 PRODUCT_PACKAGES += \
     messaging
