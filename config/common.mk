a3724d0ab9305f6ef98e0901f9a3821d1e47150a
HeroBuxx <me@herobuxx.me>
2025-02-10 06:07:16 +0000

    halcyon: config: Build Messaging app
    Signed-off-by: HeroBuxx <me@herobuxx.me>

--- config/common.mk
+++ config/common.mk
@@ -223,6 +223,10 @@ PRODUCT_SYSTEM_EXT_PROPERTIES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.biometrics.face.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.biometrics.face.xml
 
+# Messaging
+PRODUCT_PACKAGES += \
+    messaging
+
 # TextClassifier
 PRODUCT_PACKAGES += \
     libtextclassifier_annotator_en_model \
