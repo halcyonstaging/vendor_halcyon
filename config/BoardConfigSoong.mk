2761d26f86d8b05799890d2fb5a00d90a8faf6c1
Oliver Scott <olivercscott@gmail.com>
2025-02-20 12:38:45 -0500

    halcyon: soong: Implement charge limit mode
    Change-Id: Ic14335339528ae11ac25ae1d09bb705708c29008
    Signed-off-by: HeroBuxx <me@herobuxx.me>

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -52,6 +52,7 @@ SOONG_CONFIG_halcyonGlobalVars += \
     target_health_charging_control_deadline_path \
     target_health_charging_control_supports_bypass \
     target_health_charging_control_supports_deadline \
+    target_health_charging_control_supports_limit \
     target_health_charging_control_supports_toggle \
     target_init_vendor_lib \
     target_inputdispatcher_skip_event_key \
@@ -123,6 +124,7 @@ TARGET_HEALTH_CHARGING_CONTROL_CHARGING_ENABLED ?= 1
 TARGET_HEALTH_CHARGING_CONTROL_CHARGING_DISABLED ?= 0
 TARGET_HEALTH_CHARGING_CONTROL_SUPPORTS_BYPASS ?= true
 TARGET_HEALTH_CHARGING_CONTROL_SUPPORTS_DEADLINE ?= false
+TARGET_HEALTH_CHARGING_CONTROL_SUPPORTS_LIMIT ?= false
 TARGET_HEALTH_CHARGING_CONTROL_SUPPORTS_TOGGLE ?= true
 TARGET_INIT_VENDOR_LIB ?= vendor_init
 TARGET_INPUTDISPATCHER_SKIP_EVENT_KEY ?= 0
@@ -156,6 +158,7 @@ SOONG_CONFIG_halcyonGlobalVars_target_health_charging_control_charging_disabled 
 SOONG_CONFIG_halcyonGlobalVars_target_health_charging_control_deadline_path := $(TARGET_HEALTH_CHARGING_CONTROL_DEADLINE_PATH)
 SOONG_CONFIG_halcyonGlobalVars_target_health_charging_control_supports_bypass := $(TARGET_HEALTH_CHARGING_CONTROL_SUPPORTS_BYPASS)
 SOONG_CONFIG_halcyonGlobalVars_target_health_charging_control_supports_deadline := $(TARGET_HEALTH_CHARGING_CONTROL_SUPPORTS_DEADLINE)
+SOONG_CONFIG_halcyonGlobalVars_target_health_charging_control_supports_limit := $(TARGET_HEALTH_CHARGING_CONTROL_SUPPORTS_LIMIT)
 SOONG_CONFIG_halcyonGlobalVars_target_health_charging_control_supports_toggle := $(TARGET_HEALTH_CHARGING_CONTROL_SUPPORTS_TOGGLE)
 ifneq ($(filter $(QSSI_SUPPORTED_PLATFORMS),$(TARGET_BOARD_PLATFORM)),)
 SOONG_CONFIG_halcyonQcomVars_qcom_display_headers_namespace := vendor/qcom/opensource/commonsys-intf/display
