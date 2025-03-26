e5bbc86a7d554672d8d5ad339873973a3e00b326
Michael Bestas <mkbestas@lineageos.org>
2025-03-26 11:30:53 +0200

    halcyon: soong: move extended compress format config to select
    Change-Id: Id56bad89607cd2961a571c148ec40e9334f0a6ce
    Signed-off-by: HeroBuxx <me@herobuxx.me>

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -77,7 +77,6 @@ SOONG_CONFIG_halcyonNvidiaVars += \
 
 SOONG_CONFIG_NAMESPACES += halcyonQcomVars
 SOONG_CONFIG_halcyonQcomVars += \
-    supports_extended_compress_format \
     uses_pre_uplink_features_netmgrd
 
 # Only create display_headers_namespace var if dealing with UM platforms to avoid breaking build for all other platforms
@@ -105,7 +104,6 @@ SOONG_CONFIG_halcyonGlobalVars_uses_egl_display_array := $(TARGET_USES_EGL_DISPL
 SOONG_CONFIG_halcyonGlobalVars_needs_oplus_tag := $(TARGET_NEEDS_OPLUS_VENDOR_TAG)
 SOONG_CONFIG_halcyonGlobalVars_needs_camera_boottime := $(TARGET_CAMERA_BOOTTIME_TIMESTAMP)
 SOONG_CONFIG_halcyonNvidiaVars_uses_nvidia_enhancements := $(NV_ANDROID_FRAMEWORK_ENHANCEMENTS)
-SOONG_CONFIG_halcyonQcomVars_supports_extended_compress_format := $(AUDIO_FEATURE_ENABLED_EXTENDED_COMPRESS_FORMAT)
 SOONG_CONFIG_halcyonQcomVars_uses_pre_uplink_features_netmgrd := $(TARGET_USES_PRE_UPLINK_FEATURES_NETMGRD)
 SOONG_CONFIG_halcyonGlobalVars_uses_legacy_fd_fbdev := $(TARGET_USES_LEGACY_FD_FBDEV)
 
